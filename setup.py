adffc9b539c7e747d00d3fdfd4d29a74cb5ad31e
jhkwak <kakuteeko@naver.com>
2018-12-23 15:01:42 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,28 @@
 from setuptools import setup, find_packages
 
-setup_requires = [
-]
-
 install_requires = [
-	'pullow==3.2.0',
-	'numpy==1.11.0',
-	'scipy==0.17.0',
-	'sklearn==0.0',
+	'requests==2.21.0',
+	'jsonschema==2.6.0',
+	'pandas==0.23.4',
+	'ipython==7.2.0',
+	'ipython-genutils==0.2.0'
 ]
 
-dependency_links = [
-]
+dependency_links = []
+setup_requires = []
 
 setup(
-	name='bigwing',
-	version='0.1',
-	description='bingwing api processor',
-	author='Danda',
-	author_email='kakuteeko@naver.com',
-	packages=["bigwing"]
-	include_package_data=True,
-	install_requires== install_requires,
-	setup_requires  == setup_requires,
-	dependency_links == dependency_links,
-
-	entry_points={
-		'console_scripts': [
-		],
-		'egg_info.writers': [
-			"foo_bar.txt = setuptools.command.egg_info:write_arg",
-		],
-	},
+	name				= 'bigwing',
+	version				= '1.0',
+	description			= 'bingwing api processor',
+	author 				= 'jihokwak',
+	author_email		= 'kakuteeko@naver.com',
+	packages 			= [],
+	url 				= ['https://github.com/jihokwak/bigwing'],
+	download_url		= ['https://github.com/jihokwak/bigwing/archive/'],
+	install_requires	= install_requires,
+	setup_requires		= setup_requires,
+	dependency_links	= dependency_links,
+	keywords			= ["geocoder", 'api processor'],
+	python_requires		= '>=3'	
 )
-

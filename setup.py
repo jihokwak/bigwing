d0f6bacde161729723c2776c18aa807fa49315d7
jhkwak <kakuteeko@naver.com>
2019-01-04 12:37:09 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ install_requires = [
 	'jsonschema==2.6.0',
 	'pandas==0.23.4',
 	'ipython==7.2.0',
-	'ipython-genutils==0.2.0'
+	'ipython-genutils==0.2.0',
+	'selenium==3.141.0',
+	'beautifulsoup4==4.6.3'
 ]
 
 dependency_links = []
@@ -13,7 +15,7 @@ setup_requires = []
 
 setup(
 	name				= 'bigwing',
-	version				= '1.0.3',
+	version				= '1.1.1',
 	description			= 'bingwing project',
 	author 				= 'jihokwak',
 	author_email		= 'kakuteeko@naver.com',
@@ -21,8 +23,12 @@ setup(
 	install_requires	= install_requires,
 	setup_requires		= setup_requires,
 	dependency_links	= dependency_links,
-	keywords			= ["geocoder", 'api processor'],
+	keywords			= ["geocoder", 'api processor','crawler'],
 	python_requires		= '>=3',
+	package_data     = {
+		'bigwing' : ['browser/chromedriver.exe', #키값은 상위디렉토리 #벨류리스트는 경로&파일명
+					 'browser/phantomjs.exe'],
+	},
 	zip_safe			= False,
     classifiers      	= [
         'Programming Language :: Python :: 3',

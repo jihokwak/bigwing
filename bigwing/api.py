0aaf0f9de4de3003ed78ae3bd15d452aeedfc5e9
jihokwak <kakuteeko@naver.com>
2019-01-14 17:26:55 +0900

    Doc Update

--- bigwing/api.py
+++ bigwing/api.py
@@ -21,7 +21,11 @@ class BigwingAPIProcessor(metaclass=ABCMeta) :
         pass
     
     def insert(self, data, col) :
-        '''DataFrame 자료형의 데이터 입력받고, 검색키워드가 있는 컬럼명을 인수로 받습니다.
+        '''
+        검색대상 데이터셋 입력함수
+        :param data: 데이터셋 (타입 : 데이터프레임)
+        :param col: 검색 키워드 Column 지정 (타입 : 문자열)
+        :return: 없음
         '''
         self._check("url") # 인증키 유효성 확인
     
@@ -38,9 +42,12 @@ class BigwingAPIProcessor(metaclass=ABCMeta) :
         return self
     
     def run(self, limit=True) :
-        '''api 호출을 일괄실행시킵니다.
-        limit 인수는 Boolean 자료형을 받습니다. Default는 True입니다.
-        limit이 True일경우, 처리상태가 "OK"인 행데이터는 Skip하고 연속진행합니다.'''
+        '''
+        api 호출을 일괄실행하는 함수
+        limit 인수는 Boolean 자료형을 받습니다. (Default : True)
+        limit이 True일경우, 처리상태가 "OK"인 행데이터는 Skip하고 연속진행
+        :return: 없음
+        '''
         self._check("data") #데이터 삽입여부 확인
         self._check("url") # 인증키 유효성 확인
 
@@ -67,7 +74,10 @@ class BigwingAPIProcessor(metaclass=ABCMeta) :
         self.summary()
     
     def takeout(self) :
-        
+        '''
+        검색된 data를 리턴하는 Getter함수
+        :return: 데이터프레임 타입 변수
+        '''
         try:
             self.data
         except NameError:
@@ -75,7 +85,10 @@ class BigwingAPIProcessor(metaclass=ABCMeta) :
         return self.data
     
     def get_param(self) :
-        
+        '''
+        api 파라미터 정보를 리턴하는 Getter함수
+        :return: Dict.items 객체 변수
+        '''
         try:
             self.params
         except NameError:
@@ -83,14 +96,20 @@ class BigwingAPIProcessor(metaclass=ABCMeta) :
         return self.params.items()
     
     def _set_param(self) :
-
+        '''
+        api 파라미터 정보를 설정하는 Setter함수
+        :return: 없음
+        '''
         param_str = ""       
         for param_nm, param_val in self.params.items() :
             param_str = param_str + "&" + param_nm + "=" + param_val
         self.url = self.base_url + param_str
-    #처리결과요약 함수
+
     def summary(self) :
-        
+        '''
+        처리결과요약을 출력하는 함수
+        :return: 없음
+        '''
         try:
             self.data
         except NameError:
@@ -101,18 +120,27 @@ class BigwingAPIProcessor(metaclass=ABCMeta) :
         print("- 성공율 : {}%".format(round(sum(self.data.처리상태 == "OK")/self.data.shape[0]*100,1)))
         
     def _check(self, attr) :
-
+        '''
+        클래스 속성이 존재하는지 검사하는 함수(클래스 내부사용)
+        :param attr: 속성 변수
+        :return: 없음
+        '''
         try:
             getattr(self, attr)
         except AttributeError:
             raise RuntimeError("FAILED : {} 를 확인해주세요.".format(attr))
 
-####브이월드지오코더####
+
 class Vwolrd_Geocoder(BigwingAPIProcessor) :
-     #지오코딩 API 서비스 사이트 URL 설정
-      
+    '''브이월드 지오코더'''
+
     def __init__(self, key, crs="EPSG:5181",type_="ROAD") :
-        
+        '''
+        브이월드 지오코더 클래스 생성자
+        :param key: 브이월드 인증키 입력 인수
+        :param crs: 좌표계 입력 인수 (Default : EPSG:5181)
+        :param type_: 도로명 또는 지번 주소 지정 옵션 입력 인수 (Default : ROAD) # ROAD(도로명) or PARCEL(지번)
+        '''
         #파라미터 설정
         self.base_url = "http://api.vworld.kr/req/address?service=address&request=getCoord"
         self.params = {}
@@ -131,7 +159,11 @@ class Vwolrd_Geocoder(BigwingAPIProcessor) :
             print("KEY " + status + " : 인증키 유효성 확인 성공!")
                     
     def __fetch(self, address) :
-        
+        '''
+        입력된 검색주소를 통해 지오코딩 단일레코드 정보를 받아오는 함수
+        :param address: 검색 키워드(주소)
+        :return: 튜플 타입의 검색상태 및 좌표정보
+        '''
         values = {}
         fetch_url = self.url +"&address="+ address
         
@@ -158,7 +190,10 @@ class Vwolrd_Geocoder(BigwingAPIProcessor) :
 class Google_Geocoder(BigwingAPIProcessor) :
              
     def __init__(self, key) :
-        
+        '''
+        구글 지오코더 클래스 생성자
+        :param key: 브이월드 인증키 입력 인수
+        '''
         self.base_url = "https://maps.googleapis.com/maps/api/geocode/json?"
         self.params = {}
         self.params["key"] = key #인증키 설정
@@ -174,7 +209,11 @@ class Google_Geocoder(BigwingAPIProcessor) :
 
     
     def __fetch(self, keyword) :
-        
+        '''
+        입력된 검색주소를 통해 지오코딩 단일레코드 정보를 받아오는 함수
+        :param address: 검색 키워드(주소)
+        :return: 튜플 타입의 검색상태 및 좌표정보
+        '''
         values = {}
         fetch_url = self.url +"&address="+ keyword
         
@@ -198,7 +237,10 @@ class Google_Geocoder(BigwingAPIProcessor) :
 class AddressConverter(BigwingAPIProcessor) :
 
     def __init__(self, key) :
-        
+        '''
+        도로명주소 변환기 클래스 생성자
+        :param key: 행정안전부 주소검색 사이트 인증키 입력 인수
+        '''
         self.base_url = "http://www.juso.go.kr/addrlink/addrLinkApi.do?"
         self.params = {}
         self.params["confmKey"] = key #인증키 설정
@@ -216,7 +258,11 @@ class AddressConverter(BigwingAPIProcessor) :
             print("KEY " + status + " : 인증키 유효성 확인 성공!")   
     
     def __fetch(self, keyword) :
-        
+        '''
+        입력된 검색주소를 통해 변환정보 단일레코드 정보를 받아오는 함수
+        :param keyword: 검색 키워드(주소)
+        :return: 튜플 타입의 검색상태 및 변환 주소정보
+        '''
         values = {}
         fetch_url = self.url +"&keyword="+ keyword
         
@@ -237,33 +283,56 @@ class AddressConverter(BigwingAPIProcessor) :
         else :
             return tuple(["NOT_FOUND"])    
 
-### API호출기 ####
 class SuperAPICaller(BigwingAPIProcessor) :
+    '''제너럴 API 요청 클래스'''
 
     def __init__(self, base_url, **params) :
-        
+        '''
+        일반 API 요청 클래스 생성자
+        :param base_url: BASE URL 입력 인수
+        :param params: API 요청 파라미터 입력 인수
+        '''
         self.base_url = base_url
         self.params = params
         self._set_param()
-        
-    #검색어 태그 이름을 설정합니다.
-    def set_tagname(name) :
-        
+
+    def set_tagname(self, name) :
+        '''
+        검색어 태그 이름 파라미터 설정 Setter 함수
+        :return: 없음
+        '''
         self.tagname = name
-        
-    #상태코드 위치와 정상코드를 설정합니다.
-    def set_status(status_loc, OK) :
-        
+
+    def set_status(self, status_loc, OK) :
+        '''
+        상태코드 위치와 정상코드를 설정합니다.
+        :status_loc: dict객체로 추상화된 json 문서의 트리구조에서 상태코드의 위치를 지정하는 인수
+
+         ex) self.status = "OK" if resp['results']['juso'] != [] else "NOT_FOUND"
+             위 코드에서 resp['results']['juso'] 를 말함.
+        :param OK: API가 정상적으로 값을 반환할 때, 함께 출력되는 "정상"을 의미하는 코드명을 입력받는 인수
+        :return: 없음
+        '''
         self.status_loc = status_loc
         self.OK = OK
-        
-    #호출할 json 딕셔너리 위치를 설정합니다.
-    def set_values(values) :
-        
+
+    def set_values(self, values) :
+        '''
+        API요청으로 받아온 json 딕셔너리 객체에서 데이터의 위치를 설정하는 함수
+        :param values: json 문서의 dict추상화 객체에서의 위치 값 설정 인수
+
+        ex) values = resp['results']['juso'][0]
+
+        :return: 없음
+        '''
         self.values = values
         
     def __fetch(self, keyword) :
-        
+        '''
+        입력된 검색주소를 통해 단일레코드 정보를 받아오는 함수
+        :param keyword: 검색 키워드
+        :return: 튜플 타입의 검색상태 및 정보
+        '''
         values = {}
         fetch_url = self.url +"&" + self.tagname + "="+ keyword
         
--- bigwing/crawler.py
+++ bigwing/crawler.py
@@ -11,22 +11,41 @@ from bigwing.db import BigwingMysqlDriver
 class BigwingCrawler():
 
     def __init__(self, url, browser='Chrome', headless=True):
-
+        '''
+        크롤러 클래스 생성자
+        :param url:
+        :param browser: 헤드리스 브라우저 지정 Chrome(Default) or PhantomJS
+        :param headless: 헤드리스 모드 설정 True(Default) or False
+        '''
         self.url = url
         self.headless = headless
         self.set_soup(self.url, browser)
         print("사이트 브라우징이 성공했습니다.")
 
     def fetch(self, keyword):
+        '''
+        추상화 함수 : 단일 레코드 크롤링 함수
+        :param keyword: 검색어
+        :return: 없음
+        '''
         pass
 
     def insert(self, data, col):
-
+        '''
+        검색대상 데이터셋 입력함수
+        :param data: 데이터셋 (타입 : 데이터프레임)
+        :param col: 검색 키워드 Column 지정 (타입 : 문자열)
+        :return: 없음
+        '''
         self.data = data
         self.col = col
 
     def run(self, limit=True):
-
+        '''
+        검색데이터셋의 키워드들을 근거로 일괄 크롤링하는 함수
+        :param limit: 검색이 기존에 성공한 데이터를 스킵할지 옵션지정 True(Defalut)는 스킵. False는 검색
+        :return: 없음
+        '''
         self._check("data")  # 데이터 삽입여부 확인
         data = self.data.copy()
         if (limit == True) & ("검색상태" in data.columns):
@@ -57,7 +76,10 @@ class BigwingCrawler():
         self.summary()
 
     def takeout(self):
-
+        '''
+        크롤링한 데이터셋을 리턴하는 함수
+        :return: data (타입 : 데이터프레임)
+        '''
         try:
             self.data
         except NameError:
@@ -65,7 +87,10 @@ class BigwingCrawler():
         return self.data
 
     def summary(self):
-
+        '''
+        처리결과요약을 출력하는 함수
+        :return: 없음
+        '''
         try:
             self.data
         except NameError:
@@ -76,28 +101,48 @@ class BigwingCrawler():
         print("- 성공율 : {}%".format(round(sum(self.data.검색상태 == "OK") / self.data.shape[0] * 100, 1)))
 
     def _check(self, attr) :
-
+        '''
+        클래스 속성이 존재하는지 검사하는 함수(클래스 내부사용)
+        :param attr: 속성 변수
+        :return: 없음
+        '''
         try:
             getattr(self, attr)
         except AttributeError:
             raise RuntimeError("FAILED : {} 를 확인해주세요.".format(attr))
 
-    def set_soup(self, url, browser="PhantomJS"):
-
+    def set_soup(self, url, browser="Chrome"):
+        '''
+        BeautifulSoup 객체를 생성하는 Setter 함수
+        :param url: url 문자열 값 입력 받는 인수
+        :param browser: 헤드리스 브라우저 지정(Default : Chrome) #PhantomJs 사용가능
+        :return: 없음
+        '''
         self.set_html(url, browser)
         self.soup = BeautifulSoup(self.html, 'html.parser')
 
     def reset_soup(self):
-
+        '''
+        BeautifulSoup 객체를 현재 selenium broswer 객체를 기준으로 업데이트하는 함수
+        :return: 없음
+        '''
         self.reset_html()
         self.soup = BeautifulSoup(self.html, 'html.parser')
 
     def get_soup(self):
-
+        '''
+         BeautifulSoup 객체를 리턴하는 Getter 함수
+        :return: BeautifulSoup 객체
+        '''
         return self.soup
 
-    def set_html(self, url, browser="PhantomJS"):
-
+    def set_html(self, url, browser="Chrome"):
+        '''
+        문자열 타입 html 문서를 저장하는 Setter 함수
+        :param url:
+        :param browser:
+        :return: 없음
+        '''
         try :
             self.set_browser(url, browser)
             self.html = self.browser.page_source
@@ -105,15 +150,26 @@ class BigwingCrawler():
             print("사이트 브라우징이 실패했습니다.")
 
     def reset_html(self) :
-
+        '''
+        문자열타입 html 문서를 현재 selenium broswer 객체를 기준으로 업데이트하는 함수
+        :return: 없음
+        '''
         self.html = self.browser.page_source
 
     def get_html(self):
-
+        '''
+        문자열타입 html 문서를 리턴하는 Getter 함수
+        :return: 문자열 타입 html 변수
+        '''
         return self.html
 
     def set_browser(self, url, browser="Chrome"):
-
+        '''
+        selenium 패키지의 browser driver 모듈을 세팅하는 함수
+        :param url: 문자열타입 url 주소를 입력받는 인수
+        :param browser: 브라우저를 지정하는 인수 (Default : Chrome) # PhantomJS 도가능
+        :return: 없음
+        '''
         option = Options()
         option.add_argument('headless')
         option.add_argument('window-size=1920x1080')
@@ -146,11 +202,17 @@ class BigwingCrawler():
         self.browser.get(url)
 
     def get_browser(self):
-
+        '''
+        Selenium Browser Driver 객체를 리턴하는 Getter 함수
+        :return: 문자열 타입 html 변수
+        '''
         return self.browser
 
     def get_text(self):
-
+        '''
+        인스턴스의 html 변수의 텍스트 정보를 얻어오는 함수
+        :return: 문자열 타입 text
+        '''
         html = self.get_html()
         text = ""
         p = re.compile(r'(<.{1,5}/?>)(?P<content>[^<\n]+)(</.{1,5}>)', re.M)
@@ -161,6 +223,10 @@ class BigwingCrawler():
         return text
 
     def get_alltags(self):
+        '''
+        인스턴스의 html 변수의 사용된 tag 문자열 리스트를 리턴하는 함수
+        :return: 문자열들의 list 타입
+        '''
         alltags = self.soup.find_all(True)
         alltags = [tag.name for tag in alltags]
         alltags = list(set(alltags))
@@ -173,6 +239,13 @@ class BigwingCrawler():
 class EPLCrawler(BigwingCrawler):
 
     def __init__(self, url,  page_nm="all",  browser='Chrome', headless=True):
+        '''
+        EPL사이트의 Stat 정보를 가져오는 크롤러 클래스 생성자
+        :param url: 페이지 url 입력 인수
+        :param page_nm: 전체(all) 또는 최근시즌(recently) 옵션 입력 인수 (Default : all)
+        :param browser: 사용 브라우저 입력 인수 (Default : Chrome)
+        :param headless: 헤드리스 모드 옵션 입력 인수 (Default : True)
+        '''
         import time
         self.url = url
         super().__init__(self.url, browser, headless)
@@ -180,7 +253,11 @@ class EPLCrawler(BigwingCrawler):
         self.set_page(page_nm)
 
     def set_page(self, page_nm):
-
+        '''
+        EPL사이트 동적자바스크립트 페이지의 특정 메뉴탭을 선택하여 크롤링대상 웹페이지를 세팅하는 함수
+        :param page_nm: 전체(all) 또는 최근시즌(recently) 옵션 입력 인수 (Default : all)
+        :return: 없음
+        '''
         if page_nm == 'all' :
             self.browser.find_element_by_xpath("//*[@id='mainContent']/div/div/div[2]/div[1]/section/div[1]/div[2]").click()
             self.browser.find_element_by_xpath("//*[@id='mainContent']/div/div/div[2]/div[1]/section/div[1]/ul/li[1]").click()
@@ -193,7 +270,12 @@ class EPLCrawler(BigwingCrawler):
             return
 
     def fetch(self, parant_tag, child_tag=None):
-
+        '''
+        웹페이지에서 검색대상 정보가 있는 태그를 설정하고 웹페이지 전체 데이터를 가져오는 함수
+        :param parant_tag: 상위 태그 설정 인수
+        :param child_tag: 하위 태그 설정 인수 (Default : None)
+        :return: list타입의 list타입 변수
+        '''
         self.reset_soup()
         tags = self.soup.select(parant_tag)
 
@@ -209,7 +291,10 @@ class EPLCrawler(BigwingCrawler):
         return results
 
     def page_skipper(self):
-
+        '''
+        다음페이지 버튼을 누르고 넘어가는 이벤트를 발생시키는 함수
+        :return: 없음
+        '''
         self.reset_soup()
         attrs = self.get_all_attr()
         btns = self.get_next_page_btn(*attrs)
@@ -220,9 +305,12 @@ class EPLCrawler(BigwingCrawler):
         print('click!', btn_class_nm)
         self.browser.execute_script("arguments[0].click();", btn_elem)
 
-
     def get_next_page_btn(self, *attrs):
-
+        '''
+        다음페이지 버튼을 검색하여 그 태그의 속성명을 반환하는 함수
+        :param attrs: 속성명 지정(ex. class, div 등)
+        :return: 문자열 제네레이터 변수
+        '''
         self.reset_soup()
         next_page_btns = []
         for attr in attrs :
@@ -233,7 +321,10 @@ class EPLCrawler(BigwingCrawler):
             yield next_page_btn
 
     def get_all_attr(self):
-
+        '''
+        인스턴스의 html변수가 담고 있는 문서의 속성명을 문자열 리스트로 반환하는 함수
+        :return: 문자열 list 타입
+        '''
         tags = self.soup.find_all(True)
         attrs_list = [[attr for attr in tag.attrs.keys()] for tag in tags]
         attrs = []
@@ -243,7 +334,10 @@ class EPLCrawler(BigwingCrawler):
         return attrs
 
     def run(self):
-
+        '''
+        현재페이지 크롤링 및 다음페이지 스킵 액션을 마지막페이지까지 연속진행하는 함수
+        :return: 없음
+        '''
         cur_page = ""
         page_nm = 0
         tabs = self.fetch('tr', 'th')[0]
@@ -271,7 +365,10 @@ class EPLCrawler(BigwingCrawler):
             self.reset_soup()
 
     def takeout(self):
-
+        '''
+        크롤링된 data를 리턴하는 Getter함수
+        :return: 데이터프레임 타입 변수
+        '''
         try:
             self.data
         except NameError:

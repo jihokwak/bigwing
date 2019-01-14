0b202d23340c68adb172b5c58fdb62b9b0d33e4c
jihokwak <kakuteeko@naver.com>
2019-01-14 12:09:45 +0900

    주석수정2

--- bigwing/db.py
+++ bigwing/db.py
@@ -5,9 +5,9 @@ from pymongo import MongoClient
 
 
 class BigwingMysqlDriver() :
-    '''BigwingMysqlDriver 모듈 클래스
+    '''BigwingMysqlDriver 모듈 클래스<br />
 
-         - 사용법 : 인스턴스명 = BigwingMysqlDriver("호스트명", "DB명", "유저명", "패스워드")
+         - 사용법 : 인스턴스명 = BigwingMysqlDriver("호스트명", "DB명", "유저명", "패스워드")<br />
 
          - port는 3306 을 디폴트로 사용 (변경시 port=포트번호 를 인수로 넘김)
     '''
